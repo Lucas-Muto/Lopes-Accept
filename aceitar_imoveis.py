a7884989a97a471606f58bc137a9be996b3df7e4
Lucas Muto <lucasmuto@id.uff.br>
2025-04-17 14:11:42 -0300

    recomeço

--- aceitar_imoveis.py
+++
@@ -1,3 +0,0 @@
-# Script para aceitar solicitações de imóveis automaticamente
-# Fluxo: clicar nos botões em sequência conforme os prints (1, 2, 3, 4)
-# Utiliza Selenium para automação de navegador 
